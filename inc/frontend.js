45069328f31a68b746390185f335601f026af5e0
Andrew Minion <andrew@andrewrminion.com>
2015-06-24 09:37:56 -0400

    make Ajax call on form submission

--- inc/frontend.js
+++ inc/frontend.js
@@ -1,4 +1,21 @@
-jQuery('.bible-navigation').on('submit',function(event){
-    // prevent default form submission
-    return false;
+jQuery(document).ready(function() {
+
+    jQuery('.bible-navigation').on('submit', function(event) {
+        event.preventDefault();
+        var book = jQuery('select#book').val();
+        var chapter = jQuery('select#chapter').val();
+        jQuery.ajax({
+            type: 'post',
+            dataType: 'json',
+            url: apbAjax.ajaxurl,
+            data: {
+                action: 'bible-navigate',
+                book: book,
+                chapter: chapter,
+            },
+            success: function(response) {
+                jQuery('.bible-content').html(response);
+            }
+        });
+    });
 });
