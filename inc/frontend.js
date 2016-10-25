7c3a54eff83c32657b703f876dea5c8527b4d50f
Andrew Minion <andrew@andrewrminion.com>
2016-10-25 13:02:58 -0400

    wrap JS in a closure

--- inc/frontend.js
+++ inc/frontend.js
@@ -1,44 +1,46 @@
-jQuery(document).ready(function() {
+(function(){
+    $(document).ready(function() {
 
-    // handle form via Ajax
-    jQuery('body').on('submit', 'form.bible-navigation', function(event) {
-        event.preventDefault();
-        var book = jQuery(this).children('#book').val();
-        var chapter = jQuery(this).children('#chapter').val();
-        var language = jQuery(this).children('#language').val();
-        jQuery.ajax({
-            type: 'post',
-            dataType: 'html',
-            url: apbAjax.ajaxurl,
-            data: {
-                action: 'bible_navigate',
-                book: book,
-                chapter: chapter,
-                language: language,
-            },
-            success: function(response) {
-                console.log('success!');
-                jQuery('.bible-content').fadeOut(function(){jQuery('.bible-content').html(response).fadeIn()});
-                // TODO: fade out before submission and fade in after content comes back
-                jQuery('html, body').animate({
-                    scrollTop: (jQuery('.bible-content').offset().top)
-                },500);
-                // TODO: add history.pushState and URL support
-            },
-            error: function(response) {
-                console.log('error');
-                console.log(response);
-            },
+        // handle form via Ajax
+        $('body').on('submit', 'form.bible-navigation', function(event) {
+            event.preventDefault();
+            var book = $(this).children('#book').val();
+            var chapter = $(this).children('#chapter').val();
+            var language = $(this).children('#language').val();
+            $.ajax({
+                type: 'post',
+                dataType: 'html',
+                url: apbAjax.ajaxurl,
+                data: {
+                    action: 'bible_navigate',
+                    book: book,
+                    chapter: chapter,
+                    language: language,
+                },
+                success: function(response) {
+                    console.log('success!');
+                    $('.bible-content').fadeOut(function(){$('.bible-content').html(response).fadeIn()});
+                    // TODO: fade out before submission and fade in after content comes back
+                    $('html, body').animate({
+                        scrollTop: ($('.bible-content').offset().top)
+                    },500);
+                    // TODO: add history.pushState and URL support
+                },
+                error: function(response) {
+                    console.log('error');
+                    console.log(response);
+                },
+            });
+        });
+
+        // update chapter menu on book menu change
+        $('select#book').on('change',function(){
+            var thisBook=$('select#book').val();
+            var thisBookCount=chapterCount[(thisBook-1)];
+            $('select#chapter').empty();
+            for(i=1; i<=thisBookCount; i++){
+                $('select#chapter').append('<option value="' + i + '">' + i + '</option>');
+            }
         });
     });
-
-    // update chapter menu on book menu change
-    jQuery('select#book').on('change',function(){
-        var thisBook=jQuery('select#book').val();
-        var thisBookCount=chapterCount[(thisBook-1)];
-        jQuery('select#chapter').empty();
-        for(i=1; i<=thisBookCount; i++){
-            jQuery('select#chapter').append('<option value="' + i + '">' + i + '</option>');
-        }
-    });
-});
+})(jQuery);
